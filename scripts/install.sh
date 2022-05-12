a57175a6f66b7ad269f6905bcd1d8b6e2b2f6288
aswanth-asokan <aswanth.asokan@concertidc.com>
2022-05-13 00:49:58 +0530

    add

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 set -e
-sudo chmod g+s /var/www/freightrunner-api/
-sudo chmod o-rwx /var/www/freightrunner-api/
 cd /tmp/freightrunner-api/
-sudo npm install /tmp/freightrunner-api/ 
+sudo npm i 
